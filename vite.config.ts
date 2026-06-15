c862c6a1560977c3552d8d1846ddaffc7149dffa
LaHojarasca <hojarasca37649@gmail.com>
2026-06-15 16:23:52 -0400

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true
-  },
-  build: {
-    sourcemap: true,
-    outDir: 'dist'
-  }
+  base: '/LaHojarasca/',
 });
